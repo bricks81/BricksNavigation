fdf44ccd77d9e79f2b4fdc5d29bdc7a7440bdac4
Sven Ullmann <sven.ullmann@bricks-cms.org>
2017-03-10 20:47:14 +0100

    finaly created
    basics

---
+++ config/bricks.config.php
@@ -0,0 +1,5 @@
+<?php
+
+use Bricks\Config;
+
+return [];
---
+++ src/Module.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Bricks Framework & Bricks CMS
+ * http://bricks-cms.org
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2015 bricks-cms.org
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Bricks\Navigation;
+
+use Zend\I18n\Translator\Loader\PhpArray;
+use Zend\I18n\Translator\TranslatorInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\Application;
+use Zend\Mvc\MvcEvent;
+use Zend\Navigation\Navigation;
+use Zend\Navigation\Page\AbstractPage;
+use Zend\Router\Http\TranslatorAwareTreeRouteStack;
+use Zend\View\ViewEvent;
+
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface {
+
+    const VERSION = '0.1.1';
+
+    public function getConfig(){
+        return array_replace_recursive(
+            require(__DIR__.'/../config/module.config.php'),
+            require(__DIR__.'/../config/bricks.config.php')
+        );
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__
+                ]
+            ]
+        ];
+    }
+
+    public function onBootstrap(MvcEvent $e){
+        $sm = $e->getApplication()->getServiceManager();
+        $loader = $sm->get('Bricks\Loader\Loader');
+        $router = $loader->singleton('router');
+        $routeMatch = $router->match($e->getRequest());
+        foreach($loader->singleton('backend-navigation') AS $page){
+            $this->routeMatch($page,$routeMatch);
+            $this->pageRouter($page,$router);
+        }
+    }
+
+    public function routeMatch($page,$routeMatch){
+        $page->setRouteMatch($routeMatch);
+        if(isset($page->pages)){
+            foreach($page->pages AS $page){
+                $this->routeMatch($page,$routeMatch);
+            }
+        }
+    }
+
+    public function pageRouter($page,$router){
+        $page->setRouter($router);
+        if(isset($page->pages)){
+            foreach($page->pages AS $page){
+                $this->pageRouter($page,$router);
+            }
+        }
+    }
+
+}
